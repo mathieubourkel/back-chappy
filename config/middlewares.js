61757063024874b1c5b77b2f88573458e8cd7220
Mathieu Bourkel <mathieu.bourkel@gmail.com>
2023-12-08 13:54:32 +0100

    add deploy

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,15 +1,17 @@
 module.exports = [
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: '*',
-      origin: '*'
-    }
-  },
+  // 'strapi::cors',
+   {
+     name: 'strapi::cors',
+     config: {
+       enabled: true,
+       headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
+       origin: '*',
+       methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+  //     keepHeaderOnError: true,
+     }
+   },
   'strapi::poweredBy',
   'strapi::logger',
   'strapi::query',
