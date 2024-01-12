2a28b53df32d702d2d7708e7ffd9efed0a57793f
Mathieu Bourkel <mathieu.bourkel@gmail.com>
2024-01-12 17:50:51 +0100

    aa build

--- build/config/redis.config.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.redisConfig = void 0;
-exports.redisConfig = {
-    url: "redis://127.0.0.1:6379"
-};
-//# sourceMappingURL=redis.config.js.map
--- build/enums/status.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Status = void 0;
-exports.Status = [
-    { value: 0, label: "En cours" },
-    { value: 1, label: "En attente" },
-    { value: 2, label: "Terminé" }
-];
-//# sourceMappingURL=status.enum.js.map
