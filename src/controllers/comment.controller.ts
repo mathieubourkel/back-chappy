c4086d40f52a73eb6451e5db4ff9c730bbdd97b2
Laetitia Ashry <ashry.laetitia@gmail.com>
2024-01-18 17:00:05 +0100

    Back commentaires

--- src/controllers/comment.controller.ts
+++ src/controllers/comment.controller.ts
@@ -2,6 +2,14 @@ import { NextFunction, Request, Response } from "express";
 import { Comment } from "../entities/comment.entity";
 import { Service } from "../services/Service";
 import { GlobalController } from "./controller";
+import {
+  CreateCommentDto, ModifyCommentDto
+} from "../dto/comment.dto";
+import {
+  validate,
+  ValidationError
+} from "class-validator";
+import {CustomError} from "../utils/CustomError";
 
 export class CommentController extends GlobalController {
 
@@ -23,13 +31,33 @@ export class CommentController extends GlobalController {
 
   async create(req:Request, res:Response, next:NextFunction):Promise<void> {
     await this.handleGlobal(req, res, next, async ():Promise<unknown> => {
-      return this.commentService.create(req.body)
+      const comment:CreateCommentDto = new CreateCommentDto(req.body);
+      comment.author = req.user.userId;
+
+      console.log(comment)
+      const errors:ValidationError[] = await validate(comment, { whitelist:true });
+      if (errors.length > 0) {
+        throw new CustomError("PC-DTO-CHECK", 400);
+      }
+      return this.commentService.create(comment)
     });
   };
 
   async update(req:Request, res:Response, next: NextFunction):Promise<void> {
     await this.handleGlobal(req, res, next, async ():Promise<any>=> {
-      return this.commentService.update(+req.params.id, req.body)
+      const upToUpdate:any = await this.commentService.getOneById(+req.params.id, ["author"], {id:true, author: {id: true}});
+
+      if (!upToUpdate) throw new CustomError("PC-NFC-CHECK", 400);
+      if (req.user.userId !== upToUpdate.author.id) throw new CustomError("PC-NAU-CHECK", 403);
+
+      const comment:any = new ModifyCommentDto(req.body);
+      const errors:ValidationError[] = await validate(comment, {whitelist: true});
+
+      if (errors.length > 0 ) {
+        throw new CustomError("PC-DTO-CHECK", 400);
+      }
+
+      return this.commentService.update(upToUpdate.id, comment)
     });
   };
 
