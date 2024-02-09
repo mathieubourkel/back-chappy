626cc99ca4e33c21991ab90413c7f6ae6a6d5340
mathieubourkel <mathieu.bourkel@gmail.com>
2024-02-09 18:50:40 +0100

    ajout cours for deploy

--- src/utils/cors.options.utils.ts
+++ src/utils/cors.options.utils.ts
@@ -1,5 +1,5 @@
 export const corsOptions = {
-    origin: `${process.env.VITE_PROTOCOL}://${process.env.FRONT_HOST}${process.env.FRONT_PORT != '80' && process.env.FRONT_PORT != '443' ? ":" + process.env.FRONT_PORT : ''}`,
+    origin: [`${process.env.VITE_PROTOCOL}://${process.env.FRONT_HOST}${process.env.FRONT_PORT != '80' && process.env.FRONT_PORT != '443' ? ":" + process.env.FRONT_PORT : ''}`, process.env.FINAL_URL_PROD],
     methods: 'GET,PUT,POST,DELETE,OPTIONS,PATCH,HEAD',
     credentials: true,
     allowedHeaders: 'Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization',
