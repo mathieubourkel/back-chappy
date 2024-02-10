2e1e5b66cc643a3d2bccec1bb6fba8ad0aeced32
mathieubourkel <mathieu.bourkel@gmail.com>
2024-02-10 17:08:43 +0100

    ssl

--- src/index.ts
+++ src/index.ts
@@ -4,39 +4,48 @@ import { Request, Response, NextFunction } from "express";
 import { AppDataSource } from "./data-source";
 import { Routes } from "./routes/";
 import { applyMiddlewares } from "./middlewares/manage.middlewares";
-import fs from "fs"
+import fs from "fs";
 
 const app = express();
-const options = {
-  key: fs.readFileSync(`${__dirname}/express.pem`),
-  cert: fs.readFileSync(`${__dirname}/express.cert`),
-};
 
-const server = https.createServer(options, app);
-const routeClass = new Routes()
+const routeClass = new Routes();
 routeClass.applyGlobalMiddleware(app);
 
 AppDataSource.initialize()
   .then(async () => {
     routeClass.routes.forEach((objRoute) => {
-      const { method, route, action, controller} = objRoute;
+      const { method, route, action, controller } = objRoute;
       (app as any)[method](
         route,
 
-        async (req: Request, res: Response, next: NextFunction ) => {
-          await applyMiddlewares(req, res, next, objRoute)
+        async (req: Request, res: Response, next: NextFunction) => {
+          await applyMiddlewares(req, res, next, objRoute);
         },
 
-        async (req: Request, res: Response, next: NextFunction ) => {
+        async (req: Request, res: Response, next: NextFunction) => {
           new (controller as any)()[action](req, res, next);
-        },
-
+        }
       );
-    })
-    await routeClass.applyGlobalErrorMiddleware(app)
+    });
+    await routeClass.applyGlobalErrorMiddleware(app);
   })
   .catch((error) => console.log("Unable to start DB", error));
 
-server.listen(process.env.VITE_BACK_PORT, () => {
-  console.log(`${process.env.NODE_ENV} : Server Up on this URL : ${process.env.VITE_PROTOCOL}://${process.env.VITE_BACK_HOST}:${process.env.VITE_BACK_PORT}`);
-});
+if (process.env.NODE_ENV === "production") {
+  const options = {
+    key: fs.readFileSync(`${__dirname}/certs/express.pem`),
+    cert: fs.readFileSync(`${__dirname}/certs/express.cert`),
+  };
+  const server = https.createServer(options, app);
+  server.listen(process.env.VITE_BACK_PORT, () => {
+    console.log(
+      `${process.env.NODE_ENV} : HTTPS Server Up on this URL : ${process.env.VITE_PROTOCOL}://${process.env.VITE_BACK_HOST}:${process.env.VITE_BACK_PORT}`
+    );
+  });
+} else {
+  app.listen(process.env.VITE_BACK_PORT, () => {
+    console.log(
+      `${process.env.NODE_ENV} : Server Up on this URL : ${process.env.VITE_PROTOCOL}://${process.env.VITE_BACK_HOST}:${process.env.VITE_BACK_PORT}`
+    );
+  });
+}
