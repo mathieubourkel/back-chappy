f2585d1ce5d01d8a618ef4d9382fc16d44260d80
mathieubourkel <mathieu.bourkel@gmail.com>
2024-02-10 18:01:54 +0100

    modif variables

--- src/utils/cors.options.utils.ts
+++ src/utils/cors.options.utils.ts
@@ -1,5 +1,8 @@
 export const corsOptions = {
-    origin: [`${process.env.VITE_PROTOCOL}://${process.env.FRONT_HOST}${process.env.FRONT_PORT != '80' && process.env.FRONT_PORT != '443' ? ":" + process.env.FRONT_PORT : ''}`, process.env.FINAL_URL_PROD],
+    origin: [
+      `${process.env.VITE_PROTOCOL}://${process.env.FRONT_HOST}:${process.env.FRONT_PORT}`,
+      `${process.env.VITE_PROTOCOL}://${process.env.FRONT_HOST}`,
+        ],
     methods: 'GET,PUT,POST,DELETE,OPTIONS,PATCH,HEAD',
     credentials: true,
     allowedHeaders: 'Origin, X-Api-Key, X-Requested-With, Content-Type, Accept, Authorization',
